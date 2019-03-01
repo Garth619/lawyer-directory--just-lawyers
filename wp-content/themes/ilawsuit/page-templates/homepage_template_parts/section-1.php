9f174dc22843f3fa71478769dbb892503b19ff14
Garrett Cullen <garrettwcullen@gmail.com>
2019-03-01 14:44:01 -0800

    push

--- wp-content/themes/ilawsuit/page-templates/homepage_template_parts/section-1.php
+++ wp-content/themes/ilawsuit/page-templates/homepage_template_parts/section-1.php
@@ -4,6 +4,47 @@
 			
 			<h1>Find the best lawyers near you</h1>
 			
+			<div class="sec_one_search_wrapper">
+				
+				<div class="sec_one_input_wrapper">
+				
+				<div class="input_wrapper">
+				
+					<input type="text" placeholder="Firm or Attorney Name" value="" />
+				
+				</div><!-- input_wrapper -->
+				
+				<div class="input_wrapper">
+					
+					<div class="sec_one_select_wrapper">
+						
+						<div class="sec_one_select">
+							
+							<span class="select_text">Type of Law</span><!-- select_text -->
+							
+						</div><!-- sec_one_select -->
+						
+					</div><!-- sec_one_select_wrapper -->
+					
+				</div><!-- input_wrapper -->
+				 
+				<div class="input_wrapper">
+					
+				 <input type="text" placeholder="City, State or Zip Code" value="" />
+				
+				</div><!-- input_wrapper -->
+				
+				</div><!-- sec_one_input_wrapper -->
+				 
+				<div class="sec_one_submit_wrapper">
+				 
+				 <input type="submit" value="Click to find a lawyer">
+				
+				</div><!-- sec_one_submit_wrapper -->
+				
+			
+			</div><!-- sec_one_search_wrapper -->
+			
 		</div><!-- sec_one_inner -->
 	
 		<img class="sec_one_bg" src="<?php bloginfo('template_directory');?>/images/hero-desk.jpg"/>
