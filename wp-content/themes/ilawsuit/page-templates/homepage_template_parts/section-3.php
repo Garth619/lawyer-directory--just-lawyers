7f52adfcf663c92b42ccdc233a741440f299853f
Garrett Cullen <garrettwcullen@gmail.com>
2019-03-04 13:23:47 -0800

    push

---
+++ wp-content/themes/ilawsuit/page-templates/homepage_template_parts/section-3.php
@@ -0,0 +1,19 @@
+<section id="section_three">
+	
+	<div class="sec_three_inner">
+		
+		<h2>Refine Your Search</h2>
+		
+		<div class="sec_three_tabs">
+			
+			<div class="sec_three_tab sec_three_area_tab">Area of Law</div><!-- sec_three_tab -->
+			
+			<div class="sec_three_tab sec_three_state_tab">State</div><!-- sec_three_tab -->
+				
+			<div class="sec_three_tab sec_three_city_tab">City</div><!-- sec_three_tab -->
+			
+		</div><!-- sec_three_tabs -->
+		
+	</div><!-- sec_three_inner -->
+	
+</section><!-- section_three -->
